d8e5cf1a1867f9d6d0b3f94d59fb6ae10bc45b30
unknown <oscarsteggink@icloud.com>
2025-11-04 15:14:54 +0100

    doodscherm en geluid

--- fither.py
+++ fither.py
@@ -67,22 +67,6 @@ class Fighter():
         scaled_image = pygame.transform.scale(self.image, (150, 150))
         surface.blit(image, self.rect)
         scaled_image = pygame.transform.scale(self.image, (150, 150))
-
-    #def load_images(self, folder_path):
-        #for filename in sorted(os.listdir(folder_path)):
-          #  if filename.endswith(".png"):
-                #img = pygame.image.load(os.path.join(folder_path, filename)).convert_alpha()
-               # self.frames.append(img)
-
-    #def update(self):
-        # animatie updaten
-       # self.frame_index += self.animation_speed
-       # if self.frame_index >= len(self.frames):
-        #    self.frame_index = 0
-        #self.image = self.frames[int(self.frame_index)]
-
-    #def draw(self, surface):
-        #surface.blit(self.image, self.rect)
         
 
 class Kak:
